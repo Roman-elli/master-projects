2be3788a0f81d788dba65d17da63acfb70e3b96f
Beatriz <beatriz.a.mourato@gmail.com>
2026-03-17 23:40:12 +0000

    cnn

--- advanced-machine-learning/src/config.py
+++ advanced-machine-learning/src/config.py
@@ -24,6 +24,20 @@ MLP_WEIGHT_DECAY = 0.01 # Utilizado com o otimizador ADAMW
 MLP_MOMENTUM = 0.9 # Utilizado com o otimizador SGD
 
 ## CNN ##
+CNN_EPOCHS = 50
+CNN_HIDDEN_LAYER_SIZES = [1024, 512]
+CNN_LR = 0.001
+
+# Opções de Loss: "CrossEntropy" | "MultiMarginLoss" | "CrossEntropy_LabelSmoothing"
+CNN_LOSS_FUNCTION = "CrossEntropy" 
+
+CNN_LABEL_SMOOTHING = 0.1 # Utilizado com a loss function CrossEntropy_LabelSmoothing
+
+# Opções de Otimizador: "ADAM" | "RMSprop" | "ADAMW" | "SGD"
+CNN_OPTIM = "ADAMW"
+
+CNN_WEIGHT_DECAY = 0.01 # Utilizado com o otimizador ADAMW
+CNN_MOMENTUM = 0.9 # Utilizado com o otimizador SGD
 
 ## ResNet ##
 
@@ -33,6 +47,9 @@ labels_path = Path().cwd() / ".." / "assets" / "labels"
 
 mlp_results_folder_name = f"MLP_Ep{MLP_EPOCHS}_{MLP_LOSS_FUNCTION}_{MLP_OPTIM}"
 mlp_results_path = Path().cwd() / ".." / "results" / "MLP" / mlp_results_folder_name
+
+cnn_results_folder_name = f"CNN_Ep{CNN_EPOCHS}_{CNN_LOSS_FUNCTION}_{CNN_OPTIM}"
+cnn_results_path = Path().cwd() / ".." / "results" / "CNN" / cnn_results_folder_name
 
 TRAIN_IMG_DIR = images_path / 'train'
 TRAIN_LABELS_PATH = labels_path / 'train_labels.csv'
